aab1a0dc9d8fd898d7e2fdfc634ee485085db7a2
Charles <53397655+charlesncn@users.noreply.github.com>
2021-12-07 13:59:17 +0300

    Create turing.py

---
+++ turing.py
@@ -0,0 +1,11 @@
+def find_max(nums):
+    max_num = float("-inf")
+
+    for num in nums:
+        if num > max_num:
+            max_num = num
+    return max_num
+
+nums = (3,4,5,6,9,12)
+
+print(find_max(nums))
